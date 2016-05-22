b6b8a46804bea47984bc89f2842e226880205c00
Marcel de Graaf <mail@marceldegraaf.net>
2016-05-22 23:31:11 +0200

    add documentation for Promex.Gauge.set/2

--- lib/promex/gauge.ex
+++ lib/promex/gauge.ex
@@ -4,15 +4,21 @@ defmodule Promex.Gauge do
   """
 
   @doc """
-  Set a gauge's value.
+  Set the gauge identified by `name` to the default value of 1.
 
       iex> Promex.Gauge.set("test_gauge_1")
       {:ok, 1}
+
+  """
+  def set(name), do: set(name, 1)
+
+  @doc """
+  Set the gauge identified by `name` to the value `value`.
+
       iex> Promex.Gauge.set("test_gauge_1", 10)
       {:ok, 10}
 
   """
-  def set(name), do: set(name, 1)
   def set(name, value) do
     GenServer.call(Promex.Collector, {:gauge, :set, name, value})
   end
