71aacde757a2a7875bee8c6f3da306d3bb192dc4
Marcel de Graaf <mail@marceldegraaf.net>
2016-05-27 12:37:21 +0200

    improved documentation

--- lib/promex/registry/supervisor.ex
+++ lib/promex/registry/supervisor.ex
@@ -1,6 +1,13 @@
 defmodule Promex.Registry.Supervisor do
+  @moduledoc """
+  Supervises metric registries.
+  """
+
   use Supervisor
 
+  @doc """
+  Start the registry supervisor.
+  """
   def start_link, do: Supervisor.start_link(__MODULE__, :ok, name: __MODULE__)
 
   def init(:ok) do
@@ -11,6 +18,15 @@ defmodule Promex.Registry.Supervisor do
     supervise(children, strategy: :simple_one_for_one)
   end
 
+  @doc """
+  Create a new registry called `name`. Use this same name to reference the
+  registry later on, e.g. to register a metric or collect metric values.
+
+      iex> {:ok, pid} = Promex.Registry.Supervisor.create :web
+      iex> is_pid(pid)
+      true
+
+  """
   def create(name) do
     Supervisor.start_child(__MODULE__, [name])
   end
--- test/promex/registry_supervisor_test.exs
+++ test/promex/registry_supervisor_test.exs
@@ -1,8 +1,4 @@
 defmodule Promex.Registry.SupervisorTest do
   use ExUnit.Case
-
-  test "creates a new registry" do
-    {state, _pid} = Promex.Registry.Supervisor.create(:test)
-    assert state == :ok
-  end
+  doctest Promex.Registry.Supervisor
 end
